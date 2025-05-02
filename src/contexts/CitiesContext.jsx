cc39bac9e32b138607e3630f255fbf336a9d66fb
Duy <kduy410@gmail.com>
2025-05-02 17:21:30 +0700

    update

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,7 +1,7 @@
 import { createContext, useContext, useEffect, useReducer } from "react";
 
 const CitiesContext = createContext();
-const BASE_URL = "http://localhost:8000";
+const BASE_URL = "https://worldwise-server-green.vercel.app";
 
 const initState = {
   cities: [],
